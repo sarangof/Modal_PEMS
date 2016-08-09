dce6a82da242911fcc36f5aa8ec0d12cb25abc0d
Sara Arango <sarangofc@gmail.com>
2016-08-09 12:49:18 -0400

    Reading from jotform

---
+++ survey_viz.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from jotform import JotformAPIClient
+
+# API call, import, etc
+myKey = '33dcf578e3523959b282e1bebff1f581'
+
+#def main():
+
+#jotformAPIClient = JotformAPIClient(myKey)
+#submission_filter = {"form_id":"62214117688154"}
+#submission = jotformAPIClient.get_submissions(filterArray=submission_filter) 
+
+dct = {}
+for replies in submission:
+    for questions in replies['answers']:
+        print 
+        text = replies['answers'][questions]['text']
+        answer = replies['answers'][questions]['answer']
+        dct['text']=answer
+
+    #for form in forms:
+    #    print form["title"]
+
+#if __name__ == "__main__":
+#    main()
+
+# Read obtained file
+
+
+# Clean data set
+
+
+# 
+
+pd
