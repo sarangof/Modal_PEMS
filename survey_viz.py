0eee3c6cef151431a3fa0b853f7c47bb87a90b40
Sara Arango <sarangofc@gmail.com>
2016-08-11 15:51:01 -0400

    Types of plots.

--- survey_viz.py
+++ survey_viz.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import seaborn as sns
 from jotform import JotformAPIClient
+from  datetime import datetime
 
 # API call, import, etc
 myKey = '33dcf578e3523959b282e1bebff1f581'
@@ -16,18 +18,34 @@ for replies in submission:
     for questions in replies['answers']:
         try:
             text   = replies['answers'][questions]['text']
-            type_.append(replies['answers'][questions]['type'])
+            tp = replies['answers'][questions]['type']
+            type_.append(tp)
             answer = replies['answers'][questions]['answer']
-            if text in dct.keys():
-                #print str(text.encode("utf-8"))
-                dct[text].append(answer)
+            if tp == 'control_matrix':
+                content = pd.DataFrame([]).from_dict(answer,orient='index')
+            elif tp == 'control_fullname':
+                "-".join([answer['first'],answer['middle'],answer['last']])
+            elif tp == 'control_address':
+                content = answer['addr_line1']
+            elif tp in ['control_dropdown','control_textbox','control_spinner','control_scale','control_number','control_radio']:
+                content = answer
+            elif tp == 'control_datetime':
+                b = datetime.strptime(answer['day']+answer['month']+answer['year'],'%d%M%Y')
+                a = datetime.now()
+                content = (a-b).days/365
+            elif tp == 'control_time':
+                content = datetime.strptime(answer['hourSelect']+answer['minuteSelect']+answer['ampm'],'%H%M%p')
             else:
-                dct[text] = [answer]
+
+                print tp
+                content = ['something is missing.']
+            if text in dct.keys():
+                dct[text].append(content)
+            else:
+                dct[text] = [content]
         except KeyError:
             continue
         
-    
-print(set(type_))
 
 """
 
@@ -36,6 +54,7 @@ Types:
 - control_texbox
 
 """
+
 
 
 #data = pd.DataFrame([]).from_dict(dct,orient='index')
