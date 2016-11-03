e95fca1d578742ef6a9d0800135bcf0299ca1f35
sarangof <sarangof@nyu.edu>
2016-11-03 17:58:21 -0400

    Generar grupos

--- generar_grupos.py
+++ generar_grupos.py
@@ -5,7 +5,6 @@ Created on Tue Sep 27 15:26:47 2016
 
 @author: saf537
 """
-import pandas as pd
 import re
 
 def renombrar_columnas(data):
@@ -20,7 +19,7 @@ def calcular_puntajes(data):
     """
     Aqui se implementan las "funciones" de probabilidad de usar cada uno de los modos.
     """    
-    cols = ['p12_Edad','Pendiente','Distancia','p80_16._Salario_(mensual)']
+    cols = ['p12Edad','Pendiente','Distancia','p8016_Salario_mensual']
     df = data[cols] #'18. Salario'
     df= 1./df
     df -= df.min() 
@@ -29,35 +28,50 @@ def calcular_puntajes(data):
     return data
 
 def asignar_grupos(data):
-    data['N_bicicletas'] = [x[u'Bicicletas'] for x in data[u'p15_29._¿De_cuántos_vehículos_disponen_en_su_hogar?']]
-    grupo_bici_1 = data.query("(p12_Edad<40.) & (Distancia < 7000) & (N_bicicletas > 0) & (p82_30._¿Sabe_montar_en_bicicleta?=Sí)")
-    """
-    Quiénes tienen acceso a una bicicleta? 
-    Saben andar en bicicleta? 
-    Número de personas que tienen nivel de satisfacción bajo de su modo de transporte
-    Número de personas que estarían dispuestas de cambiar su modo hacia la bicicleta
-    Percepción buena de bicicleta en cuanto a la seguridad y comodidad
-    """
-    grupo_bici_2 = data
+    data['N_bicicletas'] = [x[u'Bicicletas'] for x in data[u'p1529_De_cuntos_vehculos_disponen_en_su_hogar']]
+    grupo_bici_1 = data[(data.p12Edad<40.) 
+                    & (data.Distancia < 7000) 
+                    & (data.N_bicicletas > 0) 
+                    & (data.p8230_Sabe_montar_en_bicicleta=='Sí'.encode('utf-8'))
+                    & (data.p5412_Sufre_de_alguna_discapacidad =='No')
+                    & (data.p6732_Cul_es_su_medio_habitual_ms_frecuente_y_que_utiliza_por_ms_tiempo_en_cada_viaje_para_ir_al_trabajo != 'Bicicleta')
+                    & (data.p6833_Cul_es_su_medio_habitual_ms_frecuente_y_que_utiliza_por_ms_tiempo_en_cada_viaje_para_regresar_del_trabajo != 'Bicicleta')
+                    ]
+    grupo_bici_2da = data
+        
+    grupo_bici_3 = data[(data.p12Edad<40.) 
+                & (data.Distancia < 7000) 
+                & (data.N_bicicletas > 0) 
+                & (data.p8230_Sabe_montar_en_bicicleta=='Sí'.encode('utf-8'))
+                & (data.p5412_Sufre_de_alguna_discapacidad =='No')
+                & (data.p6732_Cul_es_su_medio_habitual_ms_frecuente_y_que_utiliza_por_ms_tiempo_en_cada_viaje_para_ir_al_trabajo != 'Bicicleta')
+                & (data.p6833_Cul_es_su_medio_habitual_ms_frecuente_y_que_utiliza_por_ms_tiempo_en_cada_viaje_para_regresar_del_trabajo != 'Bicicleta')
+                #'p3547_En_general_cunto_le_interesara_utilizar_cada_uno_de_los_siguientes_medios_1_no_le_interesa_3_inters_promedio_5_le_interesa_mucho'
+                ]
     
-    """
-    ¿Consideraría usar habitualmente un medio de transporte diferente a XX?
-    ¿Si tuviera que considerar el uso habitual un medio de transporte diferente a XXcuál utilizaría?
-    """
+    grupo_bici_4 = data
     
-    grupo_bici_3 = grupo_bici_1 = data.query("(p12_Edad<40.) & (Distancia < 7000) & (N_bicicletas > 0)")
+    grupo_tp_1 = data
     
-    #grupo_bici_4 =     
+    grupo_tp_2 = data
     
-    #grupo_tp_1 = data
+    grupo_tp_3 = data
     
-    #grupo_tp_2 = data
+    grupo_tp_4 = data
     
-    """
-    Porcentaje de personas dispuestas a montar en bicicleta
-    """
+    grupo_peaton_1 = data
+    
+    grupo_peaton_2 = data
+    
+    grupo_peaton_3 = data
+    
+    grupo_cc_1 = data
+    
+    grupo_cc_2 = data
+    
+    grupo_hf = data
+    
+    grupo_teletrabajo = data[data['p3244_Teletrabajo']=='Sí'.encode('utf-8')]
     
     
-    
-    return None
-    
+    return grupo_bici_1, grupo_bici_2da, grupo_bici_3, grupo_bici_4,grupo_tp_1, grupo_tp_2, grupo_tp_3, grupo_tp_4, grupo_peaton_1, grupo_peaton_2, grupo_peaton_3, grupo_cc_1, grupo_cc_2, grupo_hf, grupo_teletrabajo 
